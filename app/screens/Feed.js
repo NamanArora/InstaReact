70cf7e5ef13c72c397a3bab0a8660ab266af9fcc
NamanArora <naman.arora11@gmail.com>
2017-08-27 16:07:33 +0530

    TabBar changes

--- app/screens/Feed.js
+++ app/screens/Feed.js
@@ -5,6 +5,7 @@ import Add from './Add';
 import Profile from './Profile';
 import Notif from './Notif';
 import { StackNavigator, TabNavigator } from 'react-navigation';
+import { StyleSheet } from 'react-native';
 
 export const Feed= TabNavigator({
     Home:{screen: Home},
@@ -12,6 +13,15 @@ export const Feed= TabNavigator({
     Add:{screen:Add},
     Profile:{screen:Profile},
     Notif:{screen:Notif}
+},
+{
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+        
+        style: {
+            backgroundColor: '#2c3e50'
+        }
+    }
 });
 
 const styles = StyleSheet.create(
