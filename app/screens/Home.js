17a09a607b22306129f4afde58effd038d1ccef3
NamanArora <naman.arora11@gmail.com>
2017-08-27 18:28:19 +0530

    Populate dummy data

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { Text, View,StyleSheet } from 'react-native';
-
+import { Text , View,StyleSheet,BackHandler, FlatList } from 'react-native';
+import HomeItem from '../adapters/HomeItem';
 export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> textInComponent </Text>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <HomeItem username={item.key} />}
+        />
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create(
   {
