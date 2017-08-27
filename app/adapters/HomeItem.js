20ef25763c0a35d1eff013b942ef55db9054a662
NamanArora <naman.arora11@gmail.com>
2017-08-27 18:26:12 +0530

    adapters added

---
+++ app/adapters/HomeItem.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Text, Image, View, StyleSheet} from 'react-native';
+
+export default class HomeItem extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.topBar}>
+        <Text style={{color: 'white'}}>{this.props.username}</Text>
+        </View>
+        <View style={styles.photo}>
+        <Image source={require('../images/logo.png')}/>
+        </View>
+        <View style={styles.bottomBar}>
+        <Text style={{color: 'white'}}>COmments</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    
+  },
+  topBar:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  bottomBar:{
+    flex: 2,
+    flexDirection: 'row',
+    
+  },
+  photo:{
+    flex:10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
