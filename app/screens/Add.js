d9ebba833e19d5191b0d49e40a03fbd1b4db4fee
NamanArora <naman.arora11@gmail.com>
2017-09-12 03:22:51 +0530

    Camera methods

--- app/screens/Add.js
+++ app/screens/Add.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component,Dimensions }  from 'react';
+
 import { Text, View,StyleSheet } from 'react-native';
 import Camera from 'react-native-camera';
 
 export default class Add extends Component {
+
+  takePicture(){
+    this.camera.capture()
+        .then((data)=> console.log(data))
+        .catch(err => console.log(error));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        
+        <Camera
+           ref={(cam) => {
+             this.camera = cam;
+           }}
+           style={styles.preview}
+           aspect={Camera.constants.Aspect.fill}>
+           <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+       </Camera>
       </View>
     );
   }
