c3f93d89df9e7e2b6f753db7025502be6d639c42
Shahzad Mirza <Shhzdmrz@hotmail.com>
2017-10-05 16:40:46 +0400

    icons added instead of titles for tab bar navigator

--- app/screens/Feed.js
+++ app/screens/Feed.js
@@ -6,25 +6,59 @@ import Profile from './Profile';
 import Notif from './Notif';
 import { StackNavigator, TabNavigator } from 'react-navigation';
 import { StyleSheet, BackHandler } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const Feed= TabNavigator({
-    Home:{screen: Home},
-    Search:{screen: Search},
-    Add:{screen:Add},
-    Profile:{screen:Profile},
-    Notif:{screen:Notif}
+    home:{
+        screen: Home,
+        navigationOptions: {
+          tabBarIcon: ({ tintColor }) => {
+            return <Icon name="home" size={28} color={tintColor} />;}
+        },
+    },
+    Search:{
+        screen: Search,
+        navigationOptions:({navigation}) => ({
+            tabBarIcon: ({ tintColor }) => {
+                return <Icon name='search' size={30} color={ tintColor } />;
+            }        
+        }),
+    },
+    Add:{
+        screen:Add,
+        navigationOptions:({navigation}) => ({
+            tabBarIcon: ({ tintColor }) => {
+                return <Icon name='camera' size={26} color={ tintColor } />;
+            }        
+        }),
+    },
+    Profile:{
+        screen:Profile,
+        navigationOptions:({navigation}) => ({
+            tabBarIcon: ({ tintColor }) => {
+                return <Icon name='user-circle-o' size={30} color={ tintColor } />;
+            }        
+        }),
+    },
+    Notif:{
+        screen:Notif,
+        navigationOptions:({navigation}) => ({
+            tabBarIcon: ({ tintColor }) => {
+                return <Icon name='bell' size={30} color={ tintColor } />;
+            }        
+        }),
+    }
 },
 {
     tabBarPosition: 'bottom',
+    lazy: true,
     tabBarOptions: {
-            labelStyle: {
-              color: 'black',
-              fontSize: 12,
-              fontWeight: 'bold'
-            },
-        style: {
-            backgroundColor: 'white',
-        }
+      showIcon: true,
+      showLabel: false,
+      iconStyle: {
+        width: 30,
+        height: 30,
+      },
     }
 });
 
